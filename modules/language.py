0393337a7de29b5be4da5fc75322e33a4ed4c5e3
GalShachar <galshachar7688@gmail.com>
2019-05-27 16:09:53 +0300

    Languge families updated

--- modules/language.py
+++ modules/language.py
@@ -1,6 +1,6 @@
 """
-Language families:     Germanic, BaltoSlavic, Romance, NativeEnglish ,Albanian, Armenian, NonEuropean,
-                       Finno-Permic, AfroAsiatic, Turkic, Hellenic, Finnic, Karto-Zan
+Language families: Germanic, BaltoSlavic, NativeEnglish ,Other, Latin
+
 """
 
 class Language(object):
@@ -10,72 +10,61 @@ class Language(object):
         self.language_name = language_name
 
 LanguageDict = {
-        'Albania': Language(False, 'Albanian', 'Albania'),
-        'Armenia': Language(False, 'Armenian', 'Armenia'),
+        'Albania': Language(False, 'Other', 'Albania'),
+        'Armenia': Language(False, 'Other', 'Armenia'),
         'Australia': Language(True, 'NativeEnglish', 'English'),
         'Austria': Language(False, 'Germanic', 'German'),
         'Bosnia': Language(False, 'BaltoSlavic', 'Bosnia'),
         'Bulgaria': Language(False, 'BaltoSlavic', 'Bulgaria'),
         'Canada': Language(True, 'NativeEnglish', 'English'),
-        'China': Language(False, 'NonEuropean', 'China'),
+        'China': Language(False, 'Other', 'China'),
         'Croatia': Language(False, 'BaltoSlavic', 'Croatia'),
-        'Cyprus': Language(False, 'Hellenic', 'Greece'),
+        'Cyprus': Language(False, 'Other', 'Greece'),
         'Czech': Language(False, 'BaltoSlavic', 'Czech'),
         'Denmark': Language(False, 'Germanic', 'Denmark'),
-        'Estonia': Language(False, 'Finnic', 'Estonia'),
-        'Finland': Language(False, 'Finnic', 'Finland'),
-        'France': Language(False, 'Romance', 'France'),
-        'Georgia': Language(False, 'Karto-Zan', 'Georgia'),
+        'Estonia': Language(False, 'BaltoSlavic', 'Estonia'),
+        'Finland': Language(False, 'Other' , 'Finland'),
+        'France': Language(False, 'Latin', 'France'),
+        'Georgia': Language(False, 'Other', 'Georgia'),
         'Germany': Language(False, 'Germanic', 'German'),
-        'Greece': Language(False, 'Hellenic', 'Greece'),
-        'Hungary': Language(False, 'Finno-Permic', 'Hungary'),
+        'Greece': Language(False, 'Other', 'Greece'),
+        'Hungary': Language(False, 'Other', 'Hungary'),
         'Iceland': Language(False, 'Germanic', 'Iceland'),
         'Ireland': Language(True, 'NativeEnglish', 'English'),
-        'India': Language(False, 'NonEuropean', 'India'),
-        'Israel': Language(False, 'AfroAsiatic', 'Israel'),
-        'Italy': Language(False, 'Romance', 'Italy'),
+        'India': Language(False, 'Other', 'India'),
+        'Israel': Language(False, 'Other', 'Israel'),
+        'Italy': Language(False, 'Latin', 'Italy'),
         'Latvia': Language(False, 'BaltoSlavic', 'Latvia'),
         'Lithuania': Language(False, 'BaltoSlavic', 'Lithuania'),
         'Macedonia': Language(False, 'BaltoSlavic', 'Macedonia'),
-        'Malta': Language(False, 'AfroAsiatic', 'Malta'),
-        'Mexico': Language(False, 'Romance', 'Spanish'),
-        'Moldova': Language(False, 'Romance', 'Romania'),
+        'Malta': Language(False, 'Latin', 'Malta'),
+        'Mexico': Language(False, 'Latin', 'Spanish'),
+        'Moldova': Language(False, 'Latin', 'Romania'),
         'Montenegro': Language(False, 'BaltoSlavic', 'Montenegro'),
         'Netherlands': Language(False, 'Germanic', 'Netherlands'),
         'NewZealand': Language(True, 'NativeEnglish', 'English'),
-        'Norway': Language(False, 'Germanic', 'Norway'),
+        'Norway': Language(False, 'Germanic' , 'Norway'),
         'Poland': Language(False, 'BaltoSlavic', 'Poland'),
-        'Portugal': Language(False, 'Romance', 'Portugal'),
-        'Romania': Language(False, 'Romance', 'Romania'),
+        'Portugal': Language(False, 'Latin', 'Portugal'),
+        'Romania': Language(False, 'Latin', 'Romania'),
         'Russia': Language(False, 'BaltoSlavic', 'Russia'),
         'Serbia': Language(False, 'BaltoSlavic', 'Serbia'),
-        'Slovakia': Language(False, 'BaltoSlavic', 'Slovakia'),
+        'Slovakia': Language(False, 'BaltoSlavic',  'Slovakia'),
         'Slovenia': Language(False, 'BaltoSlavic', 'Slovenia'),
-        'Spain': Language(False, 'Romance', 'Spanish'),
+        'Spain': Language(False, 'Latin', 'Spanish'),
         'Sweden': Language(False, 'Germanic', 'Sweden'),
-        'Turkey': Language(False, 'Turkic', 'Turkey'),
+        'Turkey': Language(False, 'Other', 'Turkey'),
         'UK': Language(True, 'NativeEnglish', 'English'),
         'Ukraine': Language(False, 'BaltoSlavic', 'Ukraine'),
-        'US': Language(True, 'Turkic', 'English'),
-        'Vietnam': Language(False, 'NonEuropean', 'Vietnam')
+        'US': Language(True, 'NativeEnglish', 'English'),
+        'Vietnam': Language(False, 'Other', 'Vietnam')
 }
-
-'''Germanic, BaltoSlavic, Romance, NativeEnglish ,Albanian, Armenian, NonEuropean,
-                       Finno-Permic, AfroAsiatic, Turkic, Hellenic, Finnic, Karto-Zan'''
 
 # Maps language families to numbers for family classification
 FamilyToNum = {
-        'Germanic': 0,
-        'BaltoSlavic': 1,
-        'Romance': 2,
-        'NativeEnglish': 3,
-        'Albanian': 4,
-        'Armenian': 5,
-        'NonEuropean': 6,
-        'Finno-Permic': 7,
-        'AfroAsiatic': 8,
-        'Turkic': 9,
-        'Hellenic': 10,
-        'Finnic': 11,
-        'Karto-Zan': 12
+        'Germanic': 'Germanic',
+        'BaltoSlavic': 'BaltoSlavic',
+        'NativeEnglish': 'NativeEnglish',
+        'Other': 'Other',
+        'Latin': 'Latin'
 }
