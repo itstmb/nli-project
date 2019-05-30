d7733d0c8a9a7ac84fe9cfaa54223a1a8fde60e8
Tom <tombreier@gmail.com>
2019-05-30 22:19:49 +0300

    Multithread support
    Adding the ability to choose how many threads the user wants to pledge for the classification task (Allows faster classification while keeping the pc functional)

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import sys
 import globals
 
-from classifiers.char_ngrams import classifier
+from classifiers import classifier
+# from classifiers.pos import classifier
 
 
-# syntax: python main.py trichar/pos in/out binary/family/language database_directory
+# syntax: python main.py trichar/pos in/out binary/family/language #threads database_directory
 def input_interpeter(argv):
     num_components = len(argv)
-    if num_components not in {4,5}:  # only allow either 3 or 4 parameters.
+    if num_components not in {5,6}:  # only allow either 3 or 4 parameters.
         raise NameError("Input too short. please use the correct syntax.")
     else:
         if  argv[2] in {"in","out"} and argv[3] in {"binary","family","language"}:
-            if argv[1] == "trichar":
-                if num_components == 4: # load vectors
-                    classifier.char_ngrams_classifier(False, argv[2], argv[3])
-                elif num_components == 5: # generate vectors
-                    classifier.char_ngrams_classifier(True, argv[2], argv[3], argv[4])
-            elif argv[1] == "pos":
-                print("Yet to be implemented.")
+            if num_components == 5: # load vectors
+                classifier.classifier(generate_vectors=False,
+                                      feature=argv[1],
+                                      in_out_domain=argv[2],
+                                      vector_type=argv[3],
+                                      util_threads=int(argv[4]))
+            elif num_components == 6: # generate vectors
+                classifier.classifier(generate_vectors=True,
+                                      feature=argv[1],
+                                      in_out_domain=argv[2],
+                                      vector_type=argv[3],
+                                      util_threads=int(argv[4]),
+                                      database_dir=argv[5])
 
 if __name__ == '__main__':
     print ("~NLIWUGC~")
