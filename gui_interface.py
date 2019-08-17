60a61b6d313f541f200614cdadf8d95b6b5edf93
Ario <ario@maplehelios.com>
2019-08-17 14:45:49 +0300

    Feature Stacking
    Feature Stacking support, both gui and cmd now allow the user to select an additional feature and use two features for the classification task.

--- gui_interface.py
+++ gui_interface.py
@@ -14,7 +14,7 @@ HEIGHT = 500
 WIDTH = 300
 
 
-class window(tk.Frame):
+class Window(tk.Frame):
     def __init__(self, parent=None):
         canvas = tk.Canvas(parent, height=HEIGHT, width=WIDTH)
         canvas.pack()
@@ -23,9 +23,7 @@ class window(tk.Frame):
         self.pack()
         self.make_widgets(parent)
 
-
     def make_widgets(self, parent=None):
-
         main_frame = tk.Frame(parent, bg='white', height=HEIGHT, width=WIDTH)
         main_frame.place(relwidth=1, relheight=1)
 
@@ -44,33 +42,39 @@ class window(tk.Frame):
 
         # Feature selection #
         default_feature = tk.StringVar()
-        default_feature.set(setup.possible_inputs[0][0])
+        default_feature.set(setup.possible_inputs[0][1])
 
-        feature_entry = tk.OptionMenu(input_frame, default_feature, *setup.possible_inputs[0], command=callback)
+        feature_entry = tk.OptionMenu(input_frame, default_feature, *setup.possible_inputs[0][1:], command=callback)
         tk.Label(input_frame, text='Feature', bg=default_color).place(relwidth=0.2, relheight=0.1, relx=0, rely=0.05)
         feature_entry.place(relwidth=0.8, relheight=0.1, relx=0.2, rely=0.05)
         # Feature selection #
 
+        # Feature2 selection #
+        default_feature2 = tk.StringVar()
+        default_feature2.set(setup.possible_inputs[0][0])
+
+        feature2_entry = tk.OptionMenu(input_frame, default_feature2, *setup.possible_inputs[0], command=callback)
+        tk.Label(input_frame, text='Feature 2', bg=default_color).place(relwidth=0.2, relheight=0.1, relx=0, rely=0.15)
+        feature2_entry.place(relwidth=0.8, relheight=0.1, relx=0.2, rely=0.15)
+        # Feature2 selection #
 
         # Type selection #
         default_type = tk.StringVar()
         default_type.set(setup.possible_inputs[1][0])
 
         type_entry = tk.OptionMenu(input_frame, default_type, *setup.possible_inputs[1])
-        tk.Label(input_frame, text='Type', bg=default_color).place(relwidth=0.2, relheight=0.1, relx=0, rely=0.15)
-        type_entry.place(relwidth=0.8, relheight=0.1, relx=0.2, rely=0.15)
+        tk.Label(input_frame, text='Type', bg=default_color).place(relwidth=0.2, relheight=0.1, relx=0, rely=0.25)
+        type_entry.place(relwidth=0.8, relheight=0.1, relx=0.2, rely=0.25)
 
         # Type selection #
-
 
         # Domain selection #
         selected_domain = tk.IntVar()
 
-        tk.Label(input_frame, text='Domain', bg=default_color).place(relwidth=0.2, relheight=0.1, relx=0, rely=0.25)
-        tk.Radiobutton(input_frame, text=setup.possible_inputs[2][0], variable=selected_domain, value=1, indicatoron=False).place(relwidth=0.2, relheight=0.1, relx=0.2, rely=0.25)
-        tk.Radiobutton(input_frame, text=setup.possible_inputs[2][1], variable=selected_domain, value=2, indicatoron=False).place(relwidth=0.2, relheight=0.1, relx=0.4, rely=0.25)
+        tk.Label(input_frame, text='Domain', bg=default_color).place(relwidth=0.2, relheight=0.1, relx=0, rely=0.35)
+        tk.Radiobutton(input_frame, text=setup.possible_inputs[2][0], variable=selected_domain, value=1, indicatoron=False).place(relwidth=0.2, relheight=0.1, relx=0.2, rely=0.35)
+        tk.Radiobutton(input_frame, text=setup.possible_inputs[2][1], variable=selected_domain, value=2, indicatoron=False).place(relwidth=0.2, relheight=0.1, relx=0.4, rely=0.35)
         # Domain selection #
-
 
         # Threads selection #
         default_threads = tk.IntVar(parent)
@@ -78,8 +82,8 @@ class window(tk.Frame):
 
         threads_entry = tk.Spinbox(input_frame, from_= 1, to=util.get_cpu_count(), textvariable=default_threads)
 
-        tk.Label(input_frame, text='Threads', bg=default_color).place(relwidth=0.2, relheight=0.1, relx=0, rely=0.35)
-        threads_entry.place(relwidth=0.2, relheight=0.1, relx=0.2, rely=0.35)
+        tk.Label(input_frame, text='Threads', bg=default_color).place(relwidth=0.2, relheight=0.1, relx=0, rely=0.45)
+        threads_entry.place(relwidth=0.2, relheight=0.1, relx=0.2, rely=0.45)
         # Threads selection #
 
         # Iterations selection #
@@ -88,11 +92,11 @@ class window(tk.Frame):
 
         iterations_entry = tk.Spinbox(input_frame, from_=1, to=1000000, textvariable=default_iterations)
 
-        tk.Label(input_frame, text='Iterations', bg=default_color).place(relwidth=0.2, relheight=0.1, relx=0, rely=0.45)
-        iterations_entry.place(relwidth=0.2, relheight=0.1, relx=0.2, rely=0.45)
+        tk.Label(input_frame, text='Iterations', bg=default_color).place(relwidth=0.2, relheight=0.1, relx=0, rely=0.55)
+        iterations_entry.place(relwidth=0.2, relheight=0.1, relx=0.2, rely=0.55)
         # Iterations selection #
 
-        # Synchronized functionwords #
+        # Synchronized function words #
         default_sync = tk.IntVar(parent)
         default_sync.set(400)
 
@@ -106,11 +110,15 @@ class window(tk.Frame):
             else:
                 sync_label.place_forget()
                 sync_entry.place_forget()
-        # Synchronized functionwords #
+        # Synchronized function words #
 
 
         def get_attributes():
             gui_feature = str(default_feature.get())
+            if default_feature2.get() == "None":
+                gui_feature2 = None
+            else:
+                gui_feature2 = str(default_feature2.get())
             gui_type = str(default_type.get())
             required_label = tk.Label(input_frame, text='REQUIRED', fg='red', font=30, bg=default_color)
             if selected_domain.get() == 1:
@@ -118,7 +126,7 @@ class window(tk.Frame):
             elif selected_domain.get() == 2:
                 gui_domain = 'out'
             else:
-                required_label.place(relwidth=0.4, relheight=0.1, relx=0.6, rely=0.25)
+                required_label.place(relwidth=0.4, relheight=0.1, relx=0.6, rely=0.35)
                 raise ValueError('Domain not specified')
 
             gui_threads = default_threads.get()
@@ -128,8 +136,13 @@ class window(tk.Frame):
             else:
                 gui_numOfFunctionwords = 0
 
-            setup.set_params(gui_feature, gui_type, gui_domain, gui_threads, gui_iterations, gui_numOfFunctionwords)
-
+            setup.set_params(var_feature=gui_feature,
+                             var_feature2=gui_feature2,
+                             var_type=gui_type,
+                             var_domain=gui_domain,
+                             var_threads=gui_threads,
+                             var_iterations=gui_iterations,
+                             var_numOfFunctionWords=gui_numOfFunctionwords)
 
         def update_log(_text):
             print(output_label.cget('text'))
@@ -137,7 +150,7 @@ class window(tk.Frame):
             app.update()
 
         def submit_button():
-            output_label.configure(text="")
+            output_label.configure(text="")  # reset output label text
             get_attributes()
             update_log('Processing vectors...')
             users, countries = provide_vectors()  # Generate vectors for the classification task
@@ -156,7 +169,7 @@ class window(tk.Frame):
                 result = classify(users, countries)
 
             update_log('Finished\n'
-                       'score: ' + "%.3f" % round(result,3))
+                       'Score: ' + "%.3f" % round(result,3))
             util.write_scores(result)
 
         submit = tk.Button(input_frame, text ='Submit', bg='white', fg='blue', command=lambda: submit_button())
@@ -167,10 +180,11 @@ class window(tk.Frame):
         output_label = tk.Label(output_frame, text=output_text.get(), anchor='n',justify='left', font=20, bg=output_color)
         output_label.place(relwidth=1, relheight=1, relx=0, rely=0)
 
-class gui_interface():
+
+class GuiInterface:
     def __init__(self):
         root = Tk()
         root.title(window_name)
         global app
-        app = window(root)
+        app = Window(root)
         app.mainloop()
